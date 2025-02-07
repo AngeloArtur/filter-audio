eddb4a636ea0abc55b6e59c584b903db010a07db
AngeloArtur <ang.vianna@gmail.com>
2025-02-06 23:06:49 -0300

    teste de áudio com o filtra_iir

--- fala_ruido_filtrado.m
+++ fala_ruido_filtrado.m
@@ -20,30 +20,29 @@ title("Usando o filtro noch e retirando a componente de 550 Hz")
 plot(f(1:T/2), abs(X(1:T/2)),W, abs(H));
 
 y = filter(b, a, x); % filtro do sinal
-#y_iir = filtra_iir(b, a, x);
+y_iir = filtra_iir(b, a, x);
 Y = fft(y); Y = Y/(T/2);
-#Y_iir = fft(y_iir); Y_iir = Y_iir/(T/2);
+Y_iir = fft(y_iir); Y_iir = Y_iir/(T/2);
 
 figure;
 subplot(2,1,1);
 plot(f(1:T/2), abs(Y(1:T/2)));  xlabel("Frequência"); ylabel("Amplitude");
 title("Aplicação do primeiro filtro normal ");
 
-#subplot(2,1,2);
-#plot(f(1:T/2), abs(Y_iir(1:T/2)));  xlabel("Frequência"); ylabel("Amplitude");
-#title("Aplicação do primeiro filtro iir ");
+subplot(2,1,2);
+plot(f(1:T/2), abs(Y_iir(1:T/2)));  xlabel("Frequência"); ylabel("Amplitude");
+title("Aplicação do primeiro filtro iir ");
 
 wc2 = 2*pi*1750/fa;
 b2 = [1 -2*cos(wc2) 1] %coeficientes da equação numerador
 a2 = [1 -2*r*cos(wc2) r^2] %coeficientes da equação denominador
 [H2, W2] = freqz(b2, a2, 512, fa); % sistema, num, num. pontos, frequência de amostragem
-subplot(2,1,2);
-plot(f(1:T/2), abs(Y(1:T/2)),W2, abs(H2));
+%subplot(2,1,2);
+%plot(f(1:T/2), abs(Y(1:T/2)),W2, abs(H2));
 
 y2 = filter(b2, a2, y); % filtro do sinal
-%y_iir2 = filtra_iir(b2, a2, y_iir);
+y_iir2 = filtra_iir(b2, a2, y_iir);
 Y2 = fft(y2); Y2 = Y2/(T/2);
-#y_iir =  filtra_iir(x, a, y_iir);
 figure;
 plot(f(1:T/2), abs(Y2(1:T/2)));  xlabel("Frequência"); ylabel("Ganho");
 title("Aplicação do segundo filtro ");
@@ -53,5 +52,4 @@ title("Aplicação do segundo filtro ");
 figure;
 plot(Z, abs(H)); xlabel("Tempo"); ylabel("Amplitude");
 title("Respota ao impulso do Filtro");
-sound(y2, fa);
-
+sound(y_iir2, fa);
